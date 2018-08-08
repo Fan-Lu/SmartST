3e5f6982ae472c204847a787bed9b73f6bb86ef7
Fan-Lu <fanlu207@gmail.com>
2018-08-08 15:33:10 -0400

    fix bug in env

--- Environment_V2/environment.py
+++ Environment_V2/environment.py
@@ -11,15 +11,16 @@ punishment = -1000
 
 
 class enviroment():
-	'''  
-	describtion: basic class, build this for load all data files
-	dir_name: to indicate the dictionary of all numpy data files. (example: dir_name = "Environment")
-	'''
+    """
+    describtion: basic class, build this for load all data files
+    dir_name: to indicate the dictionary of all numpy data files. (example: dir_name = "Environment")
+    """
+
     def __init__(self, dir_name):
         self.load_files(dir_name)
 
-    def load_files(self, dir_name): 
-	# initial function for load all data files and load mask data files.
+    def load_files(self, dir_name):
+        # initial function for load all data files and load mask data files.
         name = os.path.join(os.getcwd(), dir_name)
         a = os.listdir(name)
         files = [os.path.join(name, names) for names in a]
@@ -43,8 +44,8 @@ class enviroment():
             delattr(self, '{}'.format(i))
         setattr(self, "database", record)
 
-    def geinitupian(self, time): 
-	# interaction function for environment. Input is time and it is seconds
+    def geinitupian(self, time):
+        # interaction function for environment. Input is time and it is seconds
         if time > 2592000:
             raise ValueError('time must be within 30 days.')
         database = self.database
@@ -56,7 +57,7 @@ class enviroment():
         return result
 
     def dayin(self, array, name='tmp_file.txt'):
-	# output array, print array on a txt.file
+        # output array, print array on a txt.file
         assert array.shape.__len__() == 2, 'input of array must has shape of two'
         # path = os.path.join(os.getcwd(), name)
         # if not os.path.exists(path):
@@ -90,17 +91,17 @@ enviroment_example = enviroment('Environment_V2')
 
 class env:
     action_space = {'up': [0, 1], 'upright': [1, 1], 'right': [1, 0], 'rightdown': [1, -1], 'down': [0, -1], 'downleft':[-1, -1], 'left': [-1, 0], 'leftup': [-1, 1]}
-	'''
-	action space indicate all possible action
-	initial parameters:
-	start_loc: car initialization location , a list of two number within [0,99]
-	target: car target location , a list of two number within [0,99]
-	time: cars movement initial time, is int second.
-	alpha: modify the balance between distance reward and negative time cost reward
-	time_factor: modify the influence of negative time cost reward
-	plot: if set true, our env will show a display windows.
-	sleep: TO DO ISSUE
-	'''
+    '''
+    action space indicate all possible action
+    initial parameters:
+    start_loc: car initialization location , a list of two number within [0,99]
+    target: car target location , a list of two number within [0,99]
+    time: cars movement initial time, is int second.
+    alpha: modify the balance between distance reward and negative time cost reward
+    time_factor: modify the influence of negative time cost reward
+    plot: if set true, our env will show a display windows.
+    sleep: TO DO ISSUE
+    '''
     def __init__(self, start_loc, target, time, alpha = 0.5, time_factor= 0.1, plot = True, sleep = 0.5):
         self.start = start_loc
         self.target = target
@@ -131,7 +132,7 @@ class env:
 
 
     def reset(self, start_loc, target, time):
-	# To reset or env's location, target and time.
+        # To reset or env's location, target and time.
         self.start = start_loc
         self.target = target
         self.time = time
@@ -155,7 +156,7 @@ class env:
         return self.observation
 
     def end_my_travel(self, loc):
-	# decide if we have fall into a jam or we have approached our target.
+        # decide if we have fall into a jam or we have approached our target.
         if self.observation[0][loc[0], loc[1]] == 0 or np.sqrt(np.square(loc[0] - self.target[0])+np.square(loc[1] - self.target[1]))<3 :
             a = self.observation[0][loc[0], loc[1]]
             b = np.sqrt(np.square(loc[0] - self.target[0])+np.square(loc[1] - self.target[1]))
@@ -208,7 +209,7 @@ class env:
         return reward
 
     def target_and_loc(self, loc, width = 5, block_size=[100, 100]):
-	# translate our location into a matrix.
+        # translate our location into a matrix.
         assert type(loc) == list and loc.__len__()==2 and loc[0]>=0 and loc[0]<=(block_size[0]-1) and loc[1]>=0 and loc[1]<=(block_size[1]-1), "Input must be a list, and it must has two eligible int number"
         tmp = np.zeros([block_size[0]+width*2, block_size[1]+width*2])
         cen1 = loc[0]+width
