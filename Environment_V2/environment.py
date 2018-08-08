a8c8a24cb49285727b6a41812be5d83e7f5d23d4
pandezhao <981317572@qq.com>
2018-08-08 14:00:00 -0400

    add comment to env file

--- Environment_V2/environment.py
+++ Environment_V2/environment.py
@@ -10,35 +10,41 @@ global punishment
 punishment = -1000
 
 
-class enviroment(): 
+class enviroment():
+	'''  
+	describtion: basic class, build this for load all data files
+	dir_name: to indicate the dictionary of all numpy data files. (example: dir_name = "Environment")
+	'''
     def __init__(self, dir_name):
         self.load_files(dir_name)
 
-    def load_files(self, dir_name):
+    def load_files(self, dir_name): 
+	# initial function for load all data files and load mask data files.
         name = os.path.join(os.getcwd(), dir_name)
         a = os.listdir(name)
         files = [os.path.join(name, names) for names in a]
         for file_path in files:
-            if os.path.isfile(file_path) and file_path.endswith('.npy') and not file_path.endswith('mask.npy'):
+            if os.path.isfile(file_path) and file_path.endswith('.npy') and not file_path.endswith('mask.npy'): # load all data files
                 tmp = np.load(file_path)
                 setattr(self, file_path[-12:-4], tmp)
-            if file_path.endswith('maskV1.npy'):
+            if file_path.endswith('maskV1.npy'): # load mask file.
                 tmp1 = np.load(file_path)
                 value_point = []
                 for i in range(tmp1.shape[0]):
                     for j in range(tmp1.shape[1]):
                         if tmp1[i,j]!=0:
                             value_point.append([i,j])
-                setattr(self, "value_point", value_point)
+                setattr(self, "value_point", value_point) # value_point is all value point location
 
         record = getattr(self, "20161101")
-        for i in range(20161102, 20161131):
+        for i in range(20161102, 20161131): # concatenate all data array files into a array
             tmp = getattr(self, '{}'.format(i))
             record = np.concatenate((record, tmp), axis=0)
             delattr(self, '{}'.format(i))
         setattr(self, "database", record)
 
-    def geinitupian(self, time):
+    def geinitupian(self, time): 
+	# interaction function for environment. Input is time and it is seconds
         if time > 2592000:
             raise ValueError('time must be within 30 days.')
         database = self.database
@@ -50,6 +56,7 @@ class enviroment():
         return result
 
     def dayin(self, array, name='tmp_file.txt'):
+	# output array, print array on a txt.file
         assert array.shape.__len__() == 2, 'input of array must has shape of two'
         # path = os.path.join(os.getcwd(), name)
         # if not os.path.exists(path):
@@ -78,11 +85,22 @@ def shuchuCSV(data_array, file_name='tmp'):
         for i in range(data_array.shape[0]):
             writer.writerow([data_array[0]])
 
-global enviroment_example
+global enviroment_example # a shared global load class for all env
 enviroment_example = enviroment('Environment_V2')
 
 class env:
     action_space = {'up': [0, 1], 'upright': [1, 1], 'right': [1, 0], 'rightdown': [1, -1], 'down': [0, -1], 'downleft':[-1, -1], 'left': [-1, 0], 'leftup': [-1, 1]}
+	'''
+	action space indicate all possible action
+	initial parameters:
+	start_loc: car initialization location , a list of two number within [0,99]
+	target: car target location , a list of two number within [0,99]
+	time: cars movement initial time, is int second.
+	alpha: modify the balance between distance reward and negative time cost reward
+	time_factor: modify the influence of negative time cost reward
+	plot: if set true, our env will show a display windows.
+	sleep: TO DO ISSUE
+	'''
     def __init__(self, start_loc, target, time, alpha = 0.5, time_factor= 0.1, plot = True, sleep = 0.5):
         self.start = start_loc
         self.target = target
@@ -113,6 +131,7 @@ class env:
 
 
     def reset(self, start_loc, target, time):
+	# To reset or env's location, target and time.
         self.start = start_loc
         self.target = target
         self.time = time
@@ -136,6 +155,7 @@ class env:
         return self.observation
 
     def end_my_travel(self, loc):
+	# decide if we have fall into a jam or we have approached our target.
         if self.observation[0][loc[0], loc[1]] == 0 or np.sqrt(np.square(loc[0] - self.target[0])+np.square(loc[1] - self.target[1]))<3 :
             a = self.observation[0][loc[0], loc[1]]
             b = np.sqrt(np.square(loc[0] - self.target[0])+np.square(loc[1] - self.target[1]))
@@ -145,7 +165,7 @@ class env:
 
 
     def step(self, move):
-        # assert self.action_space.has_key(move), "move must be one of four reasonable movements"
+        # One step
         reward = self.calculate_reward(self.action_space[move])
 
         if self.plot:
@@ -188,6 +208,7 @@ class env:
         return reward
 
     def target_and_loc(self, loc, width = 5, block_size=[100, 100]):
+	# translate our location into a matrix.
         assert type(loc) == list and loc.__len__()==2 and loc[0]>=0 and loc[0]<=(block_size[0]-1) and loc[1]>=0 and loc[1]<=(block_size[1]-1), "Input must be a list, and it must has two eligible int number"
         tmp = np.zeros([block_size[0]+width*2, block_size[1]+width*2])
         cen1 = loc[0]+width
