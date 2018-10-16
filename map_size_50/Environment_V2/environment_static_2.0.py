20e0773e82f267dc2fd6fb0f74207e4367b15484
pandezhao <981317572@qq.com>
2018-10-16 19:21:41 -0400

    Completed environment 2.0

--- map_size_50/Environment_V2/environment_static_2.0.py
+++ map_size_50/Environment_V2/environment_static_2.0.py
@@ -21,10 +21,10 @@ class environment:
         """
         if type(games) == int:
             self.games = games
-        elif type(games) == list and len(games) == len(maps):
+        elif type(games) == dict and len(games) == len(maps):
             self.games = games
         else:
-            raise NameError("parameter games must be a int or a list")
+            raise NameError("parameter games must be a int or a dict")
 
         self.maps = []
         self.map_size = map_size
@@ -39,7 +39,7 @@ class environment:
             for i in range(map_size[0]):
                 for j in range(map_size[1]):
                     if tmp[i, j] != 0:
-                        value_point.append([i, j])
+                        value_point.append(np.array([i, j]))
 
             data_pool = []
             number_of_games = 0
@@ -47,23 +47,34 @@ class environment:
                 number_of_games = games
                 for i in range(games):
                     random_index = np.random.randint(0, len(value_point)-1, 2)
-                    if np.sum(np.abs(np.array(value_point[random_index[0]]) - np.array(value_point[random_index[1]]))) > 5:
+                    if np.sum(np.abs(value_point[random_index[0]] - value_point[random_index[1]])) > 5:
                         data_pool.append([value_point[random_index[0]], value_point[random_index[1]]])
                     else:
                         i -= 1
             elif type(games) == dict:
-                number_of_games = len(games[name])
-                try:
-                    for [index_0, index_1] in games[name]:
-                        assert type(index_0) == int and type(index_1) == int
-                        data_pool.append([index_0, index_1])
-                except:
-                    raise NameError("games list has a wrong format, each element in games list must be a two element list")
-            self.maps.append([tmp, data_pool, np.zeros([number_of_games, 2])])
+                if type(games[name]) == int:
+                    number_of_games = games[name]
+                    for i in range(number_of_games):
+                        random_index = np.random.randint(0, len(value_point) - 1, 2)
+                        if np.sum(np.abs(
+                                np.array(value_point[random_index[0]]) - np.array(value_point[random_index[1]]))) > 5:
+                            data_pool.append([value_point[random_index[0]], value_point[random_index[1]]])
+                        else:
+                            i -= 1
+                elif type(games[name]) == list:
+                    try:
+                        number_of_games = len(games[name])
+                        for i in games[name]:
+                            assert type(i[0]) == int and type(i[1]) == int, "element in dict must be int"
+                            data_pool.append([value_point[i[0]], value_point[i[1]]])
+                    except:
+                        raise NameError("games list has a wrong format, each element in games list must be a two element list")
+            self.maps.append([tmp, data_pool, np.zeros([number_of_games, 3])])
+            # this 3 first: current running_reward, second: accumulate running reward, third: times counted in running reward
 
         self.only_when_success = only_when_success
         if only_when_success:
-            self.success = False
+            self.success_flag = False
         self.digital = digital
         assert reward_type == "one" or reward_type == "two", "wrong 'reward type' input, must be 'one' or 'two'. "
         self.reward_type = reward_type
@@ -71,25 +82,29 @@ class environment:
         self.running_reward = running_reward
         self.running_reward_interval = running_reward_interval
         self.plot = False
-        self.root = tk.Tk()
-        self.canvas = tk.Canvas(self.root, bg="white", height=900, width=900)
-        self.canvas.pack()
+        # self.root = tk.Tk()
+        # self.canvas = tk.Canvas(self.root, bg="white", height=900, width=900)
+        # self.canvas.pack()
 
         self.map_index = None  # current map of game
         self.game_index = None  # current game of a map
+        self.current_game_start = None  # the start location in current game
+        self.current_game_target = None  # the target location in current game
         self.last_loc = None  # last time location in current game
         self.current_loc = None  # current location in current game
         self.action_space_name = ['up', 'upright', 'right', 'rightdown', 'down', 'downleft', 'left', 'leftup']
         self.action_space = {'up': [0, 1], 'upright': [1, 1], 'right': [1, 0], 'rightdown': [1, -1], 'down': [0, -1],
                              'downleft': [-1, -1], 'left': [-1, 0], 'leftup': [-1, 1]}
 
+        self.max_reward = 1
+        self.time_factor = 1
 
-    def figure_location(self, width = 5):
+    def figure_location(self, location, width = 5):
         tmp = np.zeros([self.map_size[0]+width*2, self.map_size[1]+width*2])
-        cen1 = self.current_loc[0] + width
-        cen2 = self.current_loc[1] + width
+        cen1 = location[0] + width
+        cen2 = location[1] + width
         tmp[cen1, cen2] = width * 10 + 10
-        for i in range(1, 6):
+        for i in range(1, 1+width):
             for j in range(0, i+1):
                 tmp[cen1 + j, cen2 + (i - j)] = (width - i) * 10 + 10
                 tmp[cen1 - j, cen2 + (i - j)] = (width - i) * 10 + 10
@@ -110,26 +125,46 @@ class environment:
                 img.save("Map_{}_start:{}_target:{}.jpg".format(i, start, target), "jpeg")
 
     def final_reward(self):
+        start = self.maps[self.map_index][1][self.game_index][0]
+        target = self.maps[self.map_index][1][self.game_index][1]
+        current = self.current_loc
+        total_distance = np.sqrt(np.sum(np.square(start - target)))
+        current_distance = np.sqrt(np.sum(np.square(current - target)))
         if self.reward_type == "one":
-
-            return
+            R = (total_distance - current_distance) / total_distance * self.max_reward
         elif self.reward_type == "two":
+            R = np.square((total_distance - current_distance) / total_distance * self.max_reward)
+        if self.running_reward:
+            self.maps[self.map_index][2][self.game_index, 1] += R
+            self.maps[self.map_index][2][self.game_index, 2] += 1
+            if self.maps[self.map_index][2][self.game_index, 2] >= self.running_reward_interval:
+                self.maps[self.map_index][2][self.game_index, 0] = self.maps[self.map_index][2][self.game_index, 1] / self.maps[self.map_index][2][self.game_index, 2]
+                self.maps[self.map_index][2][self.game_index, 1] = 0
+                self.maps[self.map_index][2][self.game_index, 2] = 0
+        return R
 
-            return
-
-    def time_punishment(self, location):
-
+    def time_punishment(self, location):  # calculate the time punishment
+        speed = self.maps[self.map_index][0][location]
+        return self.time_factor * 50.0 / speed
 
     def moveable_list(self):
-        tmp = self.maps[self.map_index]
+        tmp = self.maps[self.map_index][0]
         r_l = [0 for _ in range(8)]
         i = 0
-        for ele in self.action_space_name:
-            temp_loc = np.array(self.current_loc) + np.array(self.action_space[ele])
-            if temp_loc[0]<self.map_size-1 and temp_loc[0]>=0 and temp_loc[1]<self.map_size-1 and temp_loc[1]>=0 \
-                and tmp[temp_loc[0], temp_loc[1]] != 0:
-                r_l[i] = 1
-            i += 1
+        if self.way_back and self.last_loc:
+            for ele in self.action_space_name:
+                temp_loc = self.current_loc + np.array(self.action_space[ele])
+                if temp_loc != self.last_loc and temp_loc[0]<=self.map_size-1 and temp_loc[0]>=0 and \
+                        temp_loc[1]<=self.map_size-1 and temp_loc[1]>=0 and tmp[temp_loc] != 0:
+                    r_l[i] = 1
+                i += 1
+        else:
+            for ele in self.action_space_name:
+                temp_loc = self.current_loc + np.array(self.action_space[ele])
+                if temp_loc[0]<=self.map_size-1 and temp_loc[0]>=0 and temp_loc[1]<=self.map_size-1 and temp_loc[1]>=0 \
+                and tmp[temp_loc] != 0:
+                    r_l[i] = 1
+                i += 1
         return np.array(r_l, dtype='float32')
 
     def reset(self, map_number=None, game_number=None, plot=False):
@@ -139,57 +174,88 @@ class environment:
         plot: if True, we will plot our map
         """
 
-        self.last_loc = None
         if map_number and game_number:
-            self.current_loc =
-
+            self.map_index = map_number
+            self.game_index = game_number
         else:
-            if self.only_when_success and self.success:
-                map_rand_index = np.random.randint(0, len(self.maps)-1)
-                game_rand_index = np.random.randint(0, len(self.maps[1])-1)
+            if self.only_when_success:
+                if self.success_flag:
+                    self.map_index = np.random.randint(0, len(self.maps))
+                    self.game_index = np.random.randint(0, self.maps[self.map_index][2].shape[0])
+                    self.success_flag = False
+            else:
+                self.map_index = np.random.randint(0, len(self.maps))
+                self.game_index = np.random.randint(0, self.maps[self.map_index][2].shape[0])
 
-        self.plot = plot
-        if self.plot:
-            try:
-                self.canvas.delete(self.canv_img)
-            except:
-                pass
-            tmp = []
+        self.current_game_start = self.maps[self.map_index][1][self.game_index][0]
+        self.current_game_target = self.maps[self.map_index][1][self.game_index][1]
+        self.last_loc = None
+        self.current_loc = self.maps[self.map_index][1][self.game_index][0]
+
+        if self.plot and not plot:
+            self.root.destroy()
+        if not self.plot and plot:
+            self.root = tk.Tk()
+            self.canvas = tk.Canvas(self.root, bg = "white", height=900, width=900)
+            self.canvas.pack()
+
+        if plot:
+            tmp = self.maps[self.map_index][0] * 4
+            tmp[self.current_game_start] = 255
+            tmp[self.current_game_target] = 255
             tmp1 = Image.fromarray(tmp).resize((800, 800))
             img = ImageTk.PhotoImage(tmp1)
             self.canv_img = self.canvas.create_image(20, 20, anchor='nw', image=img)
             self.root.update()
-        else:
-            try:
-                self.canvas.delete(self.canv_img)
-                self.root.update()
-            except:
-                pass
-
+        self.plot = plot
 
         if self.digital:
-            return []
+            location_information = [self.current_loc, self.current_game_target]
         else:
-            return []
+            location_information = [self.figure_location(self.current_loc),
+                                    self.figure_location(self.current_game_target)]
+
+        return self.maps[self.map_index][0], location_information, self.moveable_list()
 
 
 
-    def step(self):
+    def step(self, move, last_step = False, test = False):
+        self.last_loc = self.current_loc
+        self.current_loc += np.array(self.action_space[move])
+        time_punish = self.time_punishment(self.current_loc)
 
         if self.plot:
             self.canvas.delete(self.canv_img)
-            tmp = []
+            tmp = self.maps[self.map_index][0] * 4
+            tmp[self.current_game_start] = 255
+            tmp[self.current_game_target] = 255
+            tmp[self.current_loc] = 255
             tmp1 = Image.fromarray(tmp).resize((800, 800))
             img = ImageTk.PhotoImage(tmp1)
             self.canv_img = self.canvas.create_image(20, 20, anchor='nw', image=img)
             self.root.update()
 
+        reach_target = (np.sum(np.abs(self.current_game_target - self.current_loc)) < 2)
+
+        if reach_target:
+            self.success_flag = True
+
+        if last_step or reach_target and not test:
+            final_R = self.final_reward()
+            if self.running_reward:
+                running_final_R = self.maps[self.map_index][2][self.game_index, 0]
+            else:
+                running_final_R = None
+        else:
+            final_R = None
+            running_final_R = None
 
         if self.digital:
-            return []
+            location_information = [self.current_loc, self.current_game_target]
         else:
-            return []
+            location_information = [self.figure_location(self.current_loc), self.figure_location(self.maps[self.map_index][0][self.current_game_target])]
 
+        return self.maps[self.map_index][0], location_information, self.moveable_list(), -time_punish, reach_target, final_R, running_final_R
 
 
 
