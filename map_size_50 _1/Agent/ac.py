03ce0ffea876dcaa3baeecd86fdcd62ea479732c
htkang369 <htkang@foxmail.com>
2018-10-20 16:34:34 -0400

    3 channel, write files

---
+++ map_size_50 _1/Agent/ac.py
@@ -0,0 +1,182 @@
+import os
+import sys
+sys.path.append('..')
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import math
+import torch.optim as optim
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
+                     padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, in_dim, layers, num_classes=512):
+        self.inplanes = 8
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_dim, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU()
+        self.layer1 = self._make_layer(block, 16, layers[0], stride=2)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 64, layers[3], stride=2)
+        self.fc = nn.Linear(3136, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+class Network(nn.Module):
+    def __init__(self, input_channel=3):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_channel, out_channels=8, kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.fc1 = nn.Linear(in_features=1568, out_features=256)
+        self.fc2 = nn.Linear(in_features=256, out_features=128)
+        self.bn1 = nn.BatchNorm2d(num_features=8)
+        self.bn2 = nn.BatchNorm2d(num_features=16)
+        self.bn3 = nn.BatchNorm2d(num_features=32)
+        self.bn4 = nn.BatchNorm2d(num_features=32)
+
+        self.relu = nn.ReLU()
+
+    def init_parameter(self):
+        nn.init.normal_(self.conv1.weight)
+        nn.init.normal_(self.conv2.weight)
+        nn.init.normal_(self.conv3.weight)
+        nn.init.normal_(self.conv4.weight)
+        nn.init.normal_(self.fc1.weight)
+        nn.init.normal_(self.fc2.weight)
+
+    def forward(self, data):
+        data = self.relu(self.bn1(self.conv1(data)))
+        data = self.relu(self.bn2(self.conv2(data)))
+        data = self.relu(self.bn3(self.conv3(data)))
+        data = self.relu(self.bn4(self.conv4(data)))
+        data = data.view(1, -1)
+        data = self.relu(self.fc1(data))
+        data = self.relu(self.fc2(data))
+
+        return data
+
+
+
+class Actor(nn.Module):
+    def __init__(self, A_DIM=4, num_channel=3):
+        super(Actor, self).__init__()
+        self.network = Network(num_channel)
+        self.actor_linear = nn.Linear(128, A_DIM)
+
+    def init_parameter(self):
+        self.network.init_parameter()
+        nn.init.normal_(self.actor_linear.weight)
+
+    def forward(self, input):
+        state = input
+        state = self.network(state)
+
+        probs = F.softmax(self.actor_linear(state))
+
+        return probs
+
+
+class Critic(nn.Module):
+    def __init__(self, num_channel=3):
+        super(Critic, self).__init__()
+
+        self.network = Network(num_channel)
+        self.critic_linear = nn.Linear(128, 1)
+        self.init_parameter()
+
+    def init_parameter(self):
+        self.network.init_parameter()
+        nn.init.normal_(self.critic_linear.weight)
+
+    def forward(self, input):
+        state = input
+        state = self.network(state)
+
+        value = self.critic_linear(state)
+
+        return value
+
+
+if __name__ == '__main__':
+    cx = Variable(torch.zeros(1, 256))
+    hx = Variable(torch.zeros(1, 256))
+    state = Variable(torch.randn(3, 100, 100)).view(1, 3, 100, 100)
+
+    print('test')
---
+++ map_size_50 _1/QuadraKill.py
@@ -0,0 +1,237 @@
+import os
+import sys
+import numpy as np
+from Environment_V2.environment_static_2 import environment
+import torch
+import torch
+import torch.optim as optim
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import random
+
+from Agent.ac import Actor, Critic
+
+tra_num = 5
+
+maps = []
+game = {}
+num_of_map = 5
+num_of_point = 5
+
+i = 0
+for ele in [2, 10, 18, 21, 33, 34, 36, 53, 56]:
+    maps.append("map_{}.npy".format(ele))
+    game[maps[i]] = [[105, 180], [200, 300], [80, 351], [5, 150], [50, 205]]
+    i += 1
+
+# [80, 351], [10, 95], [1, 66]
+
+# maps = ["map_{}.npy".format(i) for i in range(61, 3)]
+#
+# game = {"map_61.npy": 5,
+#         "map_54.npy": [[105, 180], [200, 300], [80, 351]]}
+
+num_channel = 3
+ENV = environment(maps=maps, map_size=(50, 50), games=game, only_when_success=True, digital=num_channel!=3,
+                  reward_type="two", way_back=False, running_reward=True, running_reward_interval=100)
+
+device = torch.device("cuda:0")
+actor = Actor(A_DIM=8, num_channel=num_channel)
+critic = Critic(num_channel=num_channel)
+success_counter = 0
+
+def normalize(s):
+    """
+    Coarse normalize
+    :param s: ndarray
+    :return: ndarray
+    """
+    for i in range(len(s)):
+        s[i] /= np.max(s[i])
+    return s
+
+a_opt = optim.Adam(actor.parameters(), lr=0.0005)
+c_opt = optim.Adam(critic.parameters(), lr=0.0005)
+
+
+action_dic = ['up', 'upright', 'right', 'rightdown', 'down', 'downleft', 'left', 'leftup']
+saved_dict = "saved_model"
+saved_fig = "saved_figure"
+
+GAMMA = 0.99
+TAU = 1.0
+EnCOEF = 0.01
+max_times = 300
+
+clip = 1
+
+if __name__ == '__main__':
+
+    file_reward = 'reward.txt'
+    file_success = 'success.txt'
+
+    if os.path.exists(file_reward):
+        os.remove(file_reward)
+
+    if os.path.exists(file_success):
+        os.remove(file_success)
+
+    actor.train()
+    critic.train()
+
+    episode = 1
+    np.random.seed(1)
+
+    p_loss = []
+    v_loss = []
+    reward_record = []
+
+    p_loss_tmp = []
+    v_loss_tmp = []
+    reward_record_tmp = []
+    plot_flag = False
+
+    while True:
+        if episode % 10 == 0:
+            plot_flag = True
+        else:
+            plot_flag = False
+
+        s, location, mask = ENV.reset(plot=plot_flag)
+        s = normalize(s)
+
+        all_rewards = []
+        all_values = []
+        all_entropies = []
+        all_lprobs = []
+
+
+        for step in range(max_times):
+            s = torch.from_numpy(np.array(s)).view(1, num_channel, 50, 50).float()
+            value = critic(s)
+            probs = actor(s)
+
+            mask = torch.from_numpy(mask).view(1, 8)
+            masked_probs = probs * mask
+
+            action = masked_probs.multinomial(1)  # 这里你没用masked_probs,所以会选出异常的方向
+            lporbs = torch.log(probs)
+            log_prob = lporbs.gather(1, action)
+            entropy = -(log_prob * probs).sum(1)
+            real_action = action_dic[int(action.cpu().data.numpy())]
+            if step == max_times-1:
+                s_, _, mask, r, success, f_r, running_mean_reward = ENV.step(real_action, last_step=True)
+            else:
+                s_, _, mask, r, success, f_r, running_mean_reward = ENV.step(real_action)
+
+            all_rewards.append(r)
+            all_values.append(value)
+            all_entropies.append(entropy)
+            all_lprobs.append(log_prob)
+
+            s = s_
+            s = normalize(s)
+
+            print(r)
+
+            if success:
+                r = f_r * 100
+                all_rewards[-1] = r
+                success_counter += 1
+                break
+
+        # record rewards
+        if episode % 10 == 0:
+            with open("reward.txt", 'a') as f_reward:
+                f_reward.write('total reward: {0}.'.format(all_rewards[-1]) + '\n')
+
+        print("Running {0} times, and successed {1} times!".format(episode, success_counter))
+
+        # record running time and success time
+        with open("success.txt", 'a') as f_success:
+            f_success.write('total time: {0}, success time: {1}'.format(episode, success_counter) + '\n')
+
+        episode += 1
+
+        R = torch.zeros(1, 1)
+        if not success:
+            s = torch.from_numpy(np.array(s)).view(1, num_channel, 50, 50).float()
+            value = critic(s)
+            R = value
+        all_values.append(R)
+        policy_loss = 0
+        value_loss = 0
+        gae = torch.zeros(1, 1)
+        for i in reversed(range(len(all_rewards))):
+            R = GAMMA * R + all_rewards[i]
+            advantage = R - all_values[i]
+            value_loss += 0.5 * advantage.pow(2)
+
+            delta_t = np.float(all_rewards[i]) + GAMMA * all_values[i+1] - all_values[i]
+
+            gae = gae * GAMMA * TAU + delta_t
+            policy_loss = policy_loss - all_lprobs[i] * gae - EnCOEF * all_entropies[i]
+
+        actor.zero_grad()
+        policy_loss.backward(retain_graph=True)
+        a_opt.step()
+        torch.nn.utils.clip_grad_norm(actor.parameters(), clip)
+
+        critic.zero_grad()
+        value_loss.backward(retain_graph=True)
+        c_opt.step()
+        torch.nn.utils.clip_grad_norm(critic.parameters(), clip)
+
+        # p_loss_tmp.append(policy_loss.data)
+        # v_loss_tmp.append(value_loss.data)
+        # reward_record_tmp.append(sum(all_rewards)/all_rewards.__len__())
+
+        # if (episode+1) % 200 == 0:
+        #     tmp_v_loss = sum(v_loss_tmp)/v_loss_tmp.__len__()
+        #     tmp_p_loss = sum(p_loss_tmp) / p_loss_tmp.__len__()
+        #     tmp_reward_record = sum(reward_record_tmp)/reward_record_tmp.__len__()
+        #     print("Episode: {}, In last 50 episode, average value loss:{},"
+        #           "average policy loss:{},average reward:{}".format(episode, tmp_v_loss,tmp_p_loss,tmp_reward_record))
+        #
+        #     v_loss.append(tmp_v_loss)
+        #     p_loss.append(tmp_p_loss)
+        #     reward_record.append(tmp_reward_record)
+        #     v_loss_tmp = []
+        #     p_loss_tmp = []
+        #     reward_record_tmp = []
+
+        #
+        if (episode + 1) % 500 == 0:
+            torch.save(actor.state_dict(), 'actor_model_{:d}.pth'.format(episode))
+            torch.save(critic.state_dict(), 'critic_model_{:d}.pth'.format(episode))
+        #
+        #     if not os.path.exists(saved_fig):
+        #         os.mkdir(saved_fig)
+        #     path = os.path.join(os.getcwd(), saved_fig)
+        #
+        #     plt.title("Policy loss for {} episode".format(episode))
+        #     plt.plot(p_loss)
+        #     plt.xlabel("every 200 episode")
+        #     plt.ylabel("every 200 episode average policy loss")
+        #     plt.savefig(os.path.join(path, "policy_loss_{}".format(episode)))
+        #     plt.close()
+        #     p_loss = []
+        #
+        #     plt.title("value loss for {} episode".format(episode))
+        #     plt.plot(v_loss)
+        #     plt.xlabel("every 200 episode")
+        #     plt.ylabel("every 200 episode average value loss")
+        #     plt.savefig(os.path.join(path, "value_loss_{}".format(episode)))
+        #     plt.close()
+        #     v_loss = []
+        #
+        #     plt.title("reward for {} episode".format(episode))
+        #     plt.plot(reward_record)
+        #     plt.xlabel("every 200 episode")
+        #     plt.ylabel("every 200 episode average reward")
+        #     plt.savefig(os.path.join(path, "reward_record_{}".format(episode)))
+        #     plt.close()
+        #     reward_record = []
